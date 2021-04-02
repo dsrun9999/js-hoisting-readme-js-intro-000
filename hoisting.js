e46ecada969b0b2ccf73255ba968230e5efb7669
Denny Srun <dennysrun@gmail.com>
2021-04-02 09:45:34 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -21,10 +21,11 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
+  
+  var name = "Kristin";
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+  
   }
 }
+sayMy();
